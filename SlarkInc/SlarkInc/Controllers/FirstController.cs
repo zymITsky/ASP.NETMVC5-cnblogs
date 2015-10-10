88b716f5240c2bb9ac57acf8d6b211917c4c111e
马鑫 <anyangmaxin@163.com>
2015-10-10 15:07:09 +0800

    ASP.NET MVC 5 入门教程 (3) 路由route

---
+++ SlarkInc/SlarkInc/Controllers/FirstController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SlarkInc.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public string Index()
+        {
+            return "<h1>This is index page</h1>";
+        }
+
+        public string Index2(string id)
+        {
+            return "This is first controller index page.<br/> Your Id is " + id;
+        }
+
+        public string Another()
+        {
+            return "This is first controller another page";
+        }
+    }
+}
