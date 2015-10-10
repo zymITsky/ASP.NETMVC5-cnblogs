184b09033403798057b6d6536797aa7b8303d146
马鑫 <anyangmaxin@163.com>
2015-10-10 17:55:20 +0800

    数据查询-创建Model

---
+++ SlarkInc/SlarkInc/Models/Worker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlarkInc.Models
+{
+    public class Worker
+    {
+        public int ID { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public Sex Sex { get; set; }
+
+        public double? Rating { get; set; }
+    }
+
+    public enum Sex
+    {
+        Male,
+        Female
+    }
+}
