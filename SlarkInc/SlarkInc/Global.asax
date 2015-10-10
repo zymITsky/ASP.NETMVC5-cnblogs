1cde2332d635c137c0b9771fed548ade5056b1d6
马鑫 <anyangmaxin@163.com>
2015-10-10 14:54:12 +0800

    ASP.NET MVC 5 入门教程 (1) 新建项目
    地址http://www.cnblogs.com/slark/p/mvc-5-get-started-create-project.html

---
+++ SlarkInc/SlarkInc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlarkInc.MvcApplication" Language="C#" %>
---
+++ SlarkInc/SlarkInc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlarkInc.Startup))]
+namespace SlarkInc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
