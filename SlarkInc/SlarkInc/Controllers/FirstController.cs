a9f6e0065a790369f60b687c1a0ae4d7664387a2
马鑫 <anyangmaxin@163.com>
2015-10-10 15:14:34 +0800

    通过ViewBag传值

--- SlarkInc/SlarkInc/Controllers/FirstController.cs
+++ SlarkInc/SlarkInc/Controllers/FirstController.cs
@@ -10,6 +10,9 @@ namespace SlarkInc.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Number = 8;
+            ViewBag.Message = "This is index Page";
+            ViewBag.Slarks = new List<string> { "Slark1", "Slark2", "Slark3" };
             return View();
         }
 
