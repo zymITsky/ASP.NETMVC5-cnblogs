20c00f8115bcd50de1738ef9ec61e9586e592bb2
马鑫 <anyangmaxin@163.com>
2015-10-10 18:18:06 +0800

    创建Controller

---
+++ SlarkInc/SlarkInc/Controllers/CompanyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SlarkInc.DAL;
+
+namespace SlarkInc.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private CompanyContext db=new CompanyContext();
+
+        // GET: Company
+        public ViewResult Index()
+        {
+            return View(db.Workers.ToList());
+        }
+    }
+}
