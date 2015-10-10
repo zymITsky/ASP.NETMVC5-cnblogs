87b661203917b537b3ed9879c78cde02cf2abfdc
马鑫 <anyangmaxin@163.com>
2015-10-10 18:08:24 +0800

    创建数据库上下文DbContext

---
+++ SlarkInc/SlarkInc/DAL/CompanyContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using SlarkInc.Models;
+
+namespace SlarkInc.DAL
+{
+    public class CompanyContext:DbContext
+    {
+        public CompanyContext() : base("CompanyContext")
+        {
+            
+        }
+
+        public DbSet<Worker> Workers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
