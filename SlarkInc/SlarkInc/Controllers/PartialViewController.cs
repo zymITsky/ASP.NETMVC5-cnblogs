8afde7bfbcf8a1add799fab58db45151f44deeb0
马鑫 <anyangmaxin@163.com>
2015-10-12 14:12:14 +0800

    简单分部视图
    Partial和RenderPartial的关系，可以这样类比：比如我们有一个string叫s，Partial和RenderPartial的关系就相当于s.ToString()和Response.Write(s.ToString())的关系一样。前者是返回内容，后者是输出内容。

---
+++ SlarkInc/SlarkInc/Controllers/PartialViewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SlarkInc.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
