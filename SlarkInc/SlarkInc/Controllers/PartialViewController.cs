aeadc63c55cea33dd25e930fbbdf1358cb1881ca
马鑫 <anyangmaxin@163.com>
2015-10-12 15:02:19 +0800

    使用ChildAction调用分部视图
    使用ChildAction调用分部视图

--- SlarkInc/SlarkInc/Controllers/PartialViewController.cs
+++ SlarkInc/SlarkInc/Controllers/PartialViewController.cs
@@ -13,5 +13,24 @@ namespace SlarkInc.Controllers
         {
             return View();
         }
+
+        [ChildActionOnly]
+        public PartialViewResult ChildAction(DateTime time)
+        {
+            string greetings = string.Empty;
+            if (time.Hour > 18)
+            {
+                greetings = "Good evening. Now is " + time.ToString("HH:mm:ss");
+            }
+            else if (time.Hour > 12)
+            {
+                greetings = "Good morning. Now is " + time.ToString("HH:mm:ss");
+            }
+            else
+            {
+                greetings = "Good morning. Now is " + time.ToString("HH:mm:ss");
+            }
+            return PartialView("ChildAction", greetings);
+        }
     }
 }
