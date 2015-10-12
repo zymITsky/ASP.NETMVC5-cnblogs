34477368d510969e109b1465549bf99033ceb62f
马鑫 <anyangmaxin@163.com>
2015-10-12 10:19:07 +0800

    排序
    排序

--- SlarkInc/SlarkInc/Controllers/CompanyController.cs
+++ SlarkInc/SlarkInc/Controllers/CompanyController.cs
@@ -12,9 +12,29 @@ namespace SlarkInc.Controllers
         private CompanyContext db=new CompanyContext();
 
         // GET: Company
-        public ViewResult Index()
+        public ViewResult Index(string sortOrder)
         {
-            return View(db.Workers.ToList());
+            ViewBag.FirstNameSortParm = string.IsNullOrWhiteSpace(sortOrder) ? "first_desc" : "";
+            ViewBag.LastNameSortParm = sortOrder == "last" ? "last_desc" : "last";
+            var workers = from w in db.Workers
+                select w;
+            switch (sortOrder)
+            {
+                case "first_desc":
+                    workers = workers.OrderByDescending(w => w.FirstName);
+                    break;
+                case "last_desc":
+                    workers = workers.OrderByDescending(w => w.LastName);
+                    break;
+                case "last":
+                    workers = workers.OrderBy(w => w.LastName);
+                    break;
+                default:
+                    workers = workers.OrderBy(w => w.FirstName);
+                    break;
+            }
+            
+            return View(workers.ToList());
         }
     }
 }
